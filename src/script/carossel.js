e601d600e5befbf8043c6ca2fcd808c7ddb383cb
Sileniz <walissonmacedo02@gmail.com>
2024-10-21 17:10:58 -0300

    carossel

--- src/script/carossel.js
+++ src/script/carossel.js
@@ -1,22 +1,33 @@
 let prevData = document.getElementById('prev_a')
 let nextData = document.getElementById('next_a')
-
+let containerSlider = document.querySelectorAll('.container_slider')
 prevData.addEventListener('click', (e) => prev(e));
 nextData.addEventListener('click', (e) => next(e))
 
 function next(e) {
     e.preventDefault()
-    if(window.location.href.includes('#image_6')){
-        window.location.href = '#image_9'
-    }if(window.location.href.includes('#image_9')) return
-    window.location.href = '#image_6'
+    if(containerSlider[0].id === 'width-zero'){
+        containerSlider[1].style.width = '0%';
+        containerSlider[1].style.opacity = '0';
+        containerSlider[1].id = 'width-zero';
+        return
+    }
+    containerSlider[0].style.width = '0%';
+    containerSlider[0].style.opacity = '0';
+    containerSlider[0].id = 'width-zero';
 }
-
 function prev(e) {
     e.preventDefault()
-    if(window.location.href.includes('#image_9')){
-        window.location.href = '#image_4'
-    }else{
-         window.location.href = '#image_1'
+    if(containerSlider[1].id === 'width-zero'){
+        containerSlider[1].style.width = '100%';
+        containerSlider[1].style.opacity = '1';
+        containerSlider[1].id = '';
+        return
     }
+    if(containerSlider[0].id === 'width-zero'){
+        containerSlider[0].style.width = '100%';
+        containerSlider[0].style.opacity = '1';
+        containerSlider[0].id = '';
+    }
+
 }
